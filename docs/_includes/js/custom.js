cb89b67fa4019da20ce1d44fb33b6d05413bc8bf
Joshua Lewis <Darkwater409@gmail.com>
2025-01-10 22:10:32 -0700

    Update custom.js
    Signed-off-by: Joshua Lewis <Darkwater409@gmail.com>

--- docs/_includes/js/custom.js
+++ docs/_includes/js/custom.js
@@ -1,12 +1,14 @@
-window.matchMedia('(prefers-color-scheme: dark)')
-      .addEventListener('change', event => {
-  if (event.matches) {
-      jtd.setTheme('dark');
-  } else {
-      jtd.setTheme('light');
-  }
-});
-
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    jtd.setTheme('dark');
+if (jdk.theme === 'auto') {
+      window.matchMedia('(prefers-color-scheme: dark)')
+            .addEventListener('change', event => {
+      if (event.matches) {
+            jtd.setTheme('dark');
+      } else {
+            jtd.setTheme('light');
+      }
+      });
+      
+      if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            jtd.setTheme('dark');
+      }
 }
